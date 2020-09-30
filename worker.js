3d8b2050ce4e23ede8d982e5417aeb2ea27884b6
Matheo Faivre <faivre42u@etu.univ-lorraine.fr>
2020-09-30 16:20:51 +0200

    "manifest worker"

---
+++ worker.js
@@ -0,0 +1,16 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(
+                [
+                    '/images/icons/cinema-128x128.png',
+                    '/images/icons/cinema-144x144.png',
+                    '/images/icons/cinema-512x512.png',
+                    'index.html',
+                    'liste.js',
+                    'style.css'
+                ]
+            );
+        })
+    );
+});
